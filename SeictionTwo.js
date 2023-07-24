854d82397ff321f91a4d76f7ea5e8ebf51d1848e
WEBLED <139395958+web-led@users.noreply.github.com>
2023-07-24 20:37:37 +0300

    Create SeictionTwo.js

---
+++ SeictionTwo.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ReactLogo from './download.jpeg'
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <h1 className='h1'>hello from react</h1>
+        <img src={ReactLogo}/>
+      </div>
+    </>
+  )
+}
+
+export default App;
